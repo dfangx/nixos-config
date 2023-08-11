518ddaed984d5c16c47e7372cebf6dde2b902f7d
dfangx <github.oxfrj2ct@bged98.anonaddy.com>
2023-08-10 22:28:52 -0400

    add thinkpad config

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
     };
     nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixneovim.url = "github:nixneovim/nixneovim";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { nixpkgs, nixpkgsStable, home-manager, ... }@inputs:
@@ -53,96 +54,122 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       nixpkgs.lib.nixosSystem {
-          # inherit system;
-          modules = [
-            {
-              nixpkgs.overlays = [
-                (final: prev: { 
-                  steam = prev.steam.override { 
-                    extraPkgs = pkgs: with pkgs; [ 
-                    ]; 
-                  }; 
-                  nbfc-linux = inputs.nbfc-linux.packages.${system}.nbfc;
-                  agenix = inputs.agenix.packages.${system}.default;
-                })
-                riverOverlay
-              ];
-            }
-            inputs.agenix.nixosModules.default
-            inputs.hyprland-package.nixosModules.default
-            ./machines/cykrotop/configuration.nix
-          ];
-        };
-        slothpi = let
-          system = "aarch64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        nixpkgs.lib.nixosSystem {
-          # inherit system;
-          modules = [
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-            {
-              nixpkgs.config.allowUnsupportedSystem = true;
-              nixpkgs.overlays = [
-                inputs.neovim-nix.overlays.${system}.default
-                (final: super: {
-                  makeModulesClosure = x:
-                  super.makeModulesClosure (x // { allowMissing = true; });
-                  agenix = inputs.agenix.packages.${system}.default;
-                  hydroxideNew = final.hydroxide.overrideAttrs(old: {
-                    version = "0.2.27";
-                  });
-                })
-                inputs.firefly.overlays.default
-              ];
-            }
-            inputs.agenix.nixosModules.default
-            inputs.firefly.nixosModules.firefly-iii
-            ./machines/slothpi/configuration.nix
-          ];
-        };
-      };
-      images.slothpi = nixosConfigurations.slothpi.config.system.build.sdImage;
-      homeConfigurations.cyrusng = let
-        system = "x86_64-linux";
-        lib = nixpkgs.lib.extend (import "${inputs.spikespaz}/lib");
-        pkgs = nixpkgs.legacyPackages.${system};
-        myOverlay = (final: prev: {
-          freetube = prev.freetube.overrideAttrs (oldAttrs: {
-            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.makeWrapper ];
-
-            postFixup = oldAttrs.postFixup + ''
-            wrapProgram $out/bin/freetube \
-            --add-flags --enable-features=UseOzonePlatform \
-            --add-flags --ozone-platform=wayland
-            '';
-          });
-          pkgsStable = nixpkgsStable.legacyPackages.${prev.system};
-        });
-      in 
-      home-manager.lib.homeManagerConfiguration {
-        inherit lib pkgs;
-
-        extraSpecialArgs = { inherit inputs; };
-
+        # inherit system;
         modules = [
           {
             nixpkgs.overlays = [
-              (_: prev: { adwaita-icon-theme-without-gnome = prev.gnome.adwaita-icon-theme.overrideAttrs (oldAttrs: { passthru = null; }); })
-              (_: prev: { adwaita-icon-theme-without-gnome = prev.gnome.adwaita-icon-theme.override      { gnome = null; gtk3 = null; }; })
+              (final: prev: { 
+                steam = prev.steam.override { 
+                  extraPkgs = pkgs: with pkgs; [ 
+                  ]; 
+                }; 
+                nbfc-linux = inputs.nbfc-linux.packages.${system}.nbfc;
+                agenix = inputs.agenix.packages.${system}.default;
+              })
               riverOverlay
-              myOverlay
-              inputs.neovim-nix.overlays.${system}.default
-              inputs.hyprland-package.overlays.default
-              inputs.hyprland-contrib.overlays.default
-              #inputs.nixneovim.overlays.default
             ];
           }
-          # inputs.hyprland.homeManagerModules.default
-          inputs.hyprland.homeManagerModules.default
-          #inputs.nixneovim.nixosModules.default
-          ./users/cyrusng/home.nix 
+          inputs.agenix.nixosModules.default
+          inputs.hyprland-package.nixosModules.default
+          ./machines/cykrotop/configuration.nix
+        ];
+      };
+      cykrotop-thinkpad = let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      nixpkgs.lib.nixosSystem {
+        # inherit system;
+        modules = [
+          {
+            nixpkgs.overlays = [
+              (final: prev: { 
+                steam = prev.steam.override { 
+                  extraPkgs = pkgs: with pkgs; [ 
+                  ]; 
+                }; 
+                nbfc-linux = inputs.nbfc-linux.packages.${system}.nbfc;
+                agenix = inputs.agenix.packages.${system}.default;
+              })
+              riverOverlay
+            ];
+          }
+          inputs.agenix.nixosModules.default
+          inputs.hyprland-package.nixosModules.default
+          inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga
+          ./machines/cykrotop/configuration.nix
+        ];
+      };
+      slothpi = let
+        system = "aarch64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      nixpkgs.lib.nixosSystem {
+        # inherit system;
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.overlays = [
+              inputs.neovim-nix.overlays.${system}.default
+              (final: super: {
+                makeModulesClosure = x:
+                super.makeModulesClosure (x // { allowMissing = true; });
+                agenix = inputs.agenix.packages.${system}.default;
+                hydroxideNew = final.hydroxide.overrideAttrs(old: {
+                  version = "0.2.27";
+                });
+              })
+              inputs.firefly.overlays.default
+            ];
+          }
+          inputs.agenix.nixosModules.default
+          inputs.firefly.nixosModules.firefly-iii
+          ./machines/slothpi/configuration.nix
         ];
       };
     };
-  }
+    images.slothpi = nixosConfigurations.slothpi.config.system.build.sdImage;
+    homeConfigurations.cyrusng = let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib.extend (import "${inputs.spikespaz}/lib");
+      pkgs = nixpkgs.legacyPackages.${system};
+      myOverlay = (final: prev: {
+        freetube = prev.freetube.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.makeWrapper ];
+
+          postFixup = oldAttrs.postFixup + ''
+          wrapProgram $out/bin/freetube \
+          --add-flags --enable-features=UseOzonePlatform \
+          --add-flags --ozone-platform=wayland
+          '';
+        });
+        pkgsStable = nixpkgsStable.legacyPackages.${prev.system};
+      });
+    in 
+    home-manager.lib.homeManagerConfiguration {
+      inherit lib pkgs;
+
+      extraSpecialArgs = { inherit inputs; };
+
+      modules = [
+        {
+          nixpkgs.overlays = [
+            (_: prev: { adwaita-icon-theme-without-gnome = prev.gnome.adwaita-icon-theme.overrideAttrs (oldAttrs: { passthru = null; }); })
+            (_: prev: { adwaita-icon-theme-without-gnome = prev.gnome.adwaita-icon-theme.override      { gnome = null; gtk3 = null; }; })
+            riverOverlay
+            myOverlay
+            inputs.neovim-nix.overlays.${system}.default
+            inputs.hyprland-package.overlays.default
+            inputs.hyprland-contrib.overlays.default
+            #inputs.nixneovim.overlays.default
+          ];
+        }
+        # inputs.hyprland.homeManagerModules.default
+        inputs.hyprland.homeManagerModules.default
+        #inputs.nixneovim.nixosModules.default
+        ./users/cyrusng/home.nix 
+      ];
+    };
+  };
+}
