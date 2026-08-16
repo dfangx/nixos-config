190959555199b4d8c3baddaa98aec6d0e8352c17
dfangx <github.oxfrj2ct@bged98.anonaddy.com>
2026-08-16 18:21:18 -0400

    comment out lsp document symbol

--- homeManagerModules/programs/nixvim/cmp.nix
+++ homeManagerModules/programs/nixvim/cmp.nix
@@ -43,9 +43,9 @@
           {
             name = "nvim_lsp";
           }
-          {
-            name = "nvim_lsp_document_symbol";
-          }
+          # {
+          #   name = "nvim_lsp_document_symbol";
+          # }
           {
             name = "nvim_lsp_signature_help";
           }
